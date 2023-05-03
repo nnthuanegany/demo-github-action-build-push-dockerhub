6cfe135b226423115e276e69528bd7501e95c2ce
nnthuanegany <thuan.nguyen@egany.com>
2023-05-03 09:26:50 +0700

    Update github workflows

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+console.log("Version 1.0.5");
+console.log("Hello world!");
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
